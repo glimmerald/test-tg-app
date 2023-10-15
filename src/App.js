3a6c7f9b32e2d3b815edfe8cbf4a8f0161ef0b04
glimmerald <glimmerald@gmail.com>
2023-10-15 12:25:02 +0300

    @

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useEffect } from 'react';
 import Header from './components/Header/Header';
 import { useTelegram } from './Hooks/useTelegram';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 
 function App() {
@@ -17,6 +20,10 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route index element={<ProductList />} />
+        <Route path='/form' element={<Form />} />
+      </Routes>
       <button onClick={onToggleButton}>toggle</button>
     </div>
   );
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import {useTelegram} from "../../Hooks/useTelegram";
+import "./Form.css";
+
+
+const Form = () => {
+    const [couch, setCouch] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const tg = useTelegram();
+
+    useEffect(() => {
+        tg.MainButton.setParams({
+            text: "Отправить данные"
+        })
+    }, []);
+
+    useEffect(() => {
+        if (!date || !time) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [date, time]);
+
+    const onChangeCouch = (e) => {
+        setCouch(e.target.value);
+    };
+
+    const onChangeDate = (e) => {
+        setDate(e.target.value);
+    };
+
+    const onChangeTime = (e) => {
+        setTime(e.target.value);
+    };
+
+    return (
+        <div className="form">
+            <h3 className="form-title">Введите ваши данные</h3>
+            <select className="select" value={couch} onChange={onChangeCouch}>
+                <option className="option">Тренер 1</option>
+                <option className="option">Тренер 2</option>
+                <option className="option">Тренер 3</option>
+                <option className="option">Тренер 4</option>
+                <option className="option">Тренер 5</option>
+            </select>
+            <input className="input" type="text" placeholder="Дата" value={date} onChange={onChangeDate} />
+            <input className="input" type="number" placeholder="Количество минут" value={time} onChange={onChangeTime} />
+        </div>
+    );
+};
+
+export default Form;
